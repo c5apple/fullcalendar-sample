4703c9184645cc9aa5233c80284585b842bfa423
c5apple <c5apple_party@yahoo.co.jp>
2020-10-18 13:25:55 +0900

    データ取得API

--- main.js
+++ main.js
@@ -22,36 +22,7 @@
         startTime: '9:00',
         endTime: '18:00',
       },
-      events: [
-        // 時間指定
-        {
-          title: '時間指定',
-          start: '2020-10-02 10:00',
-          end: '2020-10-02 20:00',
-        },
-        // 終日
-        {
-          title: '終日',
-          start: '2020-10-01',
-          end: '2020-10-03',
-          allDay: true
-        },
-        // 曜日指定繰り替えし
-        {
-          title: '曜日指定繰り返し',
-          daysOfWeek: [0, 6],
-          startTime: '12:00',
-          startTime: '12:30',
-        },
-        // 編集不可
-        {
-          title: '編集不可',
-          start: '2020-10-02 10:00',
-          end: '2020-10-02 20:00',
-          editable: false,
-          color: 'gray',
-        },
-      ],
+      events: './events.json',
       eventClick: function (info) {
         console.group('eventClick');
         console.log('Event: ', info.event.title);
